902609ba627dea17dd008d11328c67adae370ec6
Daniel Rivera <riverd@usi.ch>
2014-12-08 03:42:06 +0100

    latet working version

---
+++ Working version 08:12/routes/login/router.js
@@ -0,0 +1,60 @@
+/**
+ * Created by Daniel on 07/11/14.
+ */
+var express = require('express');
+var router = express.Router();
+var middleware =  require('../middleware');
+var mongoose = require('mongoose');
+var ObjectId = mongoose.Types.ObjectId;
+var User = mongoose.model('User');
+var config = require("../../config");
+
+var session;
+
+router.all('/', middleware.supportedMethods('GET, POST'));
+
+
+
+router.get('/', function(req, res, next) {
+
+    res.render('login');
+
+
+});
+
+router.post('/', function (req, res) {
+    var post = req.body;
+
+    var query = User.where({userName : post.username});
+
+    query.findOne(function(err, user){
+        if (err) { return err}
+        if (user) {
+
+            user.isValidPassword(post.password, function(n, isMatch){
+                if(isMatch) {
+                    req.session.user_id = user._id;
+
+                    res.redirect('/home?' + user._id);
+                } else{
+
+                    res.redirect('/signup');
+                }
+            });}
+
+
+
+
+        else{
+            res.redirect('/signup');
+        }
+
+
+    });
+
+
+});
+
+
+
+module.exports = router;
